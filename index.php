783e3a68184d568de36f3cc78bf36e9c78aca032
Younessalmi <younessalmi193@gmail.com>
2021-08-07 19:01:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creal Soul</title>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+<body id="controle">
+    
+    
+    <div class="footer_2">
+        <img style="width:110px;margin: 0% auto;" src="./img/logo.png" alt="logo">
+    </div>   
+
+
+    <header class="techno_header">
+        <nav>
+            <ul>
+                <li>Notre vision</li>
+                <li>Nos engagements</li>
+                <li>Votre projet</li>
+            </ul>
+        </nav>
+    </header>
+    
+
+    <div class="techno_1"> 
+
+        <div class="techno_2" style="box-sizing:border-box">
+
+            <h1 id="welcome">BIENVENUE SUR LE SITE DE CREASOUL</h1>
+            
+
+                
+                <p id="papa"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dobre magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                    dolor in reprehenderit in voluptate velit esse cillum dobre eu fugiat nulla pariatur. 
+                </p>
+
+
+
+                <picture style="width: 100%;">
+                    <source media="(max-width:480px)" srcset="./img/illu-mobile.jpg">
+                    <source media="(max-width:700px)" srcset="./img/illu-tablette.jpg">
+                    <img src="./img/illu.jpg" alt="Flowers" style="width: 100%;">
+                </picture>
+
+                
+
+                <h2>LOREM IPSUM DOLOR SIT AMET </h2>
+                <div><hr></div>
+
+
+                <p id="papa"> Vivamus sed dui vitae mi aliquam venenatis quis quis nulla. Nulla vitae mi at massa maximus ultrices. Vestibulum nibh sem, tristique
+                    sit amet orci et, hendrerit facilisis sem. Aliquam scelerisque tortor eu sodales feugiat. Proin nisl eros, egestas ac nunc vel, 
+                    condimentum sagittis ipsum. Proin et eros risus. Donec non nunc at ante viverra posuere sit amet sed eros. Phasellus vehicula, nisi a
+                    imperdiet hendrerit, risus nibh tempus felis, eu rutrum sapien lectus nec dolor. Fusce eleifend, nunc et interdum hendrerit, enim mi 
+                    rutrum massa, at iaculis elit mauris ac enim. 
+                </p>
+
+
+                <h3>CONSECTETUR ADIPISCING ELIT</h3>
+                <div><hr></div>
+
+
+                <p id="papa">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed lacus at sapien eleifend dapibus blandit eget negue. Aliquam venenatis
+                ut leo in cursus. Ut hendrerit erat non diam sollicitudin lobortis. Pellentesque laoreet est vitae mi rutrum, ornare auctor negue convallis. 
+                Nunc condimentum semper est, eu laoreet leo hendrerit eu. Ut lorem erat, luctus sed purus non, porttitor finibus magna. Aenean sed orci nunc. 
+                In ipsum metus, commodo sed purus id, vestibulum ullamcorper negue. <br><br>
+
+
+                <a style="color: #0cddbb;">Vivamus sed dui vitae mi aliquam venenatis quis quis nulla.</a> Nulla vitae mi at massa maximus ultrices. Vestibulum nibh sem,
+                tristique sit amet orci et, hendrerit facilisis sem. Aliquam scelerisque tortor eu sodales feugiat. Proin nisl eros, egestas ac nunc vel, condimentum sagittis
+                    ipsum. Proin et eros risus. Donec non nunc at ante viverra posuere sit amet sed eros. Phasellus vehicula, nisi a imperdiet hendrerit, risus nibh tempus 
+                felis, eu rutrum sapien lectus nec dolor. Fusce eleifend, nunc et interdum hendrerit, enim mi rutrum massa, at iaculis elit mauris ac enim. 
+                </p>
+        </div><!--
+        --><div class="techno_3" style="box-sizing:border-box">
+            <h4>LOREM IPSUM</h4>
+
+            <p id="papa">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed lacus at sapien eleifend dapibus blandit eget negue. Aliquam venenatis ut leo in cursus.
+            Ut hendrerit erat non diam sollicitudin lobortis. <br><br>
+            
+            Pellentesque laoreet est vitae mi rutrum, ornare auctor negue convallis. Nunc condimentum semper est, eu laoreet leo hendrerit eu. Ut lorem erat, luctus sed
+            purus non, porttitor finibus magna. Aenean sed orci nunc. In ipsum metus, commodo sed purus id, vestibulum ullamcorper negue. 
+            </p>
+
+            
+            
+            <h5>DOLOR SIT AMET </h5>
+            <div><hr></div>
+
+            <p id="papa">Vivamus sed dui vitae mi aliquam venenatis quis quis nulla. Nulla vitae mi at massa maximus ultrices. Vestibulum nibh sem, tristique sit amet orci et, 
+            hendrerit facilisis sem. Aliquam scelerisque tortor eu sodales feugiat. Proin nisl eros, egestas ac nunc vel, condimentum sagittis ipsum. Proin et eros 
+            risus. 
+            </p>
+
+            <h6>CONSECTETUR ADIPISCING ELIT</h6>
+            <div><hr></div>
+            <p id="papa">Vivamus sed dui vitae mi aliquam venenatis quis quis nulla. Nulla vitae mi at massa maximus ultrices. Vestibulum nibh sem, tristique sit amet orci et, 
+            hendrerit facilisis sem. Miguem scelerisque tortor eu sodales feugiat. Proin nisl eros, egestas ac nunc vel, condimentum sagittis ipsum.<br><br>
+
+            Proin et eros risus. Donec non nunc at ante viverra posuere sit amet sed eros. Phasellus vehicula, nisi a imperdiet hendrerit, risus nibh tempus felis,
+            eu rutrum sapien lectus nec dolor. Fusce eleifend, nunc et interdum hendrerit, enim mi rutrum massa, at iaculis elit mauris ac enim. 
+            </p>
+
+        </div>
+        
+
+    </div>
+    
+    <footer>
+
+        <div style="text-align:center;">
+
+            <img style="width:50px;" src="./img/footer.png" alt="logo">
+            <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta nobis culpa vero nihil delectus accusamus reiciendis assumenda eligendi eum, odio explicabo blanditiis hic! Maiores, doloremque delectus a in voluptates maxime!
+            Recusandae facilis, odio illo quibusdam alias obcaecati repellendus ullam ipsum doloribus voluptatibus aspernatur error nostrum rerum neque culpa nisi vitae praesentium provident quas! Facilis fuga quaerat culpa id quidem molestias.
+            Perferendis, a sint odit ut molestiae suscipit facilis dicta sequi possimus sit incidunt, sunt officia tempora blanditiis autem accusamus. Reiciendis nulla odit, ipsa ipsam animi eum quam quo? Voluptate, ullam?</p>
+        </div>
+        
+    </footer>
+<script src="./js/scripte.js"></script>
+</body>
+</html>
---
+++ js/scripte.js
@@ -0,0 +1,15 @@
+/*
+var chno_bghiti_men_la5ar = prompt("what color background you want");
+
+document.getElementById('controle').style.backgroundColor=chno_bghiti_men_la5ar;
+
+var chno_bghiti_men_la5ar2 = prompt("what color paragraphe you want");
+
+document.getElementsByTagName('p')[0].style.color=chno_bghiti_men_la5ar2;
+document.getElementsByTagName('p')[1].style.color=chno_bghiti_men_la5ar2;
+document.getElementsByTagName('p')[2].style.color=chno_bghiti_men_la5ar2;
+document.getElementsByTagName('p')[3].style.color=chno_bghiti_men_la5ar2;
+document.getElementsByTagName('p')[4].style.color=chno_bghiti_men_la5ar2;
+document.getElementsByTagName('p')[5].style.color=chno_bghiti_men_la5ar2;
+document.getElementsByTagName('p')[6].style.color=chno_bghiti_men_la5ar2;
+/*
